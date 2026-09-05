e35e36615027f27ad55dcb5426070eb7126b9757
anna <annasijo21@gmail.com>
2026-09-05 23:18:50 +0530

    Add arithmetic operators program

---
+++ 01-Basics/operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("\nAddition       = %d", a + b);
+    printf("\nSubtraction    = %d", a - b);
+    printf("\nMultiplication  = %d", a * b);
+    printf("\nDivision        = %d", a / b);
+    printf("\nRemainder       = %d\n", a % b);
+
+    return 0;
+}
